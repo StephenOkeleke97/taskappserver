2f70ba179a620998309adb3c4c7fbc4446e034fa
Stephen Okeleke <stephenokeleke@yahoo.com>
2022-03-23 00:58:23 -0700

    altered cookie settings

--- express/server.js
+++ express/server.js
@@ -8,7 +8,7 @@ const { User } = require("./schema/schema");
 const cors = require("cors");
 const corsOptions = {
   cors: true,
-  origin: ["https://taskappbysteve.herokuapp.com", "http://localhost:3000"],
+  origin: ["*"],
   credentials: true,
 };
 const app = express();
@@ -92,8 +92,8 @@ app.use(
     }),
     cookie: {
       maxAge: SESS_LIFETIME,
-      sameSite: "none",
-      secure: true,
+      sameSite: "lax",
+      secure: false,
     },
   })
 );
@@ -124,6 +124,7 @@ app.post("/login", redirectHome, (req, res) => {
       .then((result) => {
         if (result) {
           req.session.userId = result._id;
+          console.log(req.session.userId.valueOf());
           res.sendStatus(200);
         } else {
           res.status(401).send("Invalid username or password");
